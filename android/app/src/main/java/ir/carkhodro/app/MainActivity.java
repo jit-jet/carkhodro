8a707fe5f293bbf43dcdadbaa792fa9abbf36d96
jit-jet <rezaahadishahri2@gmail.com>
2026-09-07 15:54:25 +0330

    exit app in no internet

--- android/app/src/main/java/ir/carkhodro/app/MainActivity.java
+++ android/app/src/main/java/ir/carkhodro/app/MainActivity.java
@@ -1,6 +1,7 @@
 package ir.carkhodro.app;
 
 import android.os.Bundle;
+import androidx.activity.OnBackPressedCallback;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -8,5 +9,23 @@ public class MainActivity extends BridgeActivity {
     public void onCreate(Bundle savedInstanceState) {
         registerPlugin(NativePrintPlugin.class);
         super.onCreate(savedInstanceState);
+
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                String currentUrl = getBridge().getWebView().getUrl();
+                String connectionErrorUrl = getBridge().getErrorUrl();
+
+                if (connectionErrorUrl != null && connectionErrorUrl.equals(currentUrl)) {
+                    finish();
+                    return;
+                }
+
+                // Let Capacitor keep handling history and the website's own Back listener.
+                setEnabled(false);
+                getOnBackPressedDispatcher().onBackPressed();
+                setEnabled(true);
+            }
+        });
     }
 }
